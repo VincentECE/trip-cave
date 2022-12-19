fd0eb12008572eb42e72e406458b9e136742050f
Vince <vincent.tran.ece@gmail.com>
2022-12-19 00:05:39 -0600

    saves progress pi-server

--- pi-server/index.ts
+++ pi-server/index.ts
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
-const { userRoutes } = require('./routes');
-const { crossOrigin } = require('./middleware');
+import express from "express";
+import path from "path";
+import { userRoutes } from "./routes";
+import { crossOrigin } from "./middleware";
+require("./db");
 const app = express();
-require('./db');
 
 // app.use(express.static(path.resolve(__dirname, '../../client/build')));
 app.use(crossOrigin); // uncomment this line if running the client and server separately
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/', userRoutes);
+app.use("/", userRoutes);
 
 let port = 3001;
 
