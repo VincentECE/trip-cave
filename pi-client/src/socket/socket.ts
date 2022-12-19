aa97a82a799b57bd6a43882470557477985e7070
Vince <vincent.tran.ece@gmail.com>
2022-12-18 23:57:14 -0600

    adds connect button for socket.io

---
+++ pi-client/src/socket/socket.ts
@@ -0,0 +1,21 @@
+import { io } from "socket.io-client";
+import { useStore } from "../store";
+
+// const serverUrl: string = process.env.REACT_APP_PI_URL || 'http://localhost:3001';
+const serverUrl: string = "http://localhost:3001";
+
+export const establishConnection = () => {
+  const socket = io(serverUrl);
+
+  socket.on("connect_error", () => {
+    console.log("couldn't connect to server");
+  });
+
+  socket.on("connect", () => {
+    console.log("connected to server");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected from server");
+  });
+};
