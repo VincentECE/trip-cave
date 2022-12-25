fb424a237ec097d3a7baf5fed49b5258b2ab4fa6
Vince <vincent.tran.ece@gmail.com>
2022-12-25 17:30:04 -0600

    creates new slices in pi-client store

--- pi-client/src/store/createVisualControlSlice.ts
+++ pi-client/src/store/createVisualControlSlice.ts
@@ -3,24 +3,10 @@ import create, { StateCreator } from "zustand";
 import coffeeVideo from "../assets/Coffee.mp4";
 import skyScrapersVideo from "../assets/Skyscrapers.mp4";
 
+console.log("coffeeVideo: ", coffeeVideo);
+
 export interface VisualControlType {
-  currentVideo1: string;
-  currentVideo2: string;
-  video1Hidden: boolean;
-  video2Hidden: boolean;
-  video1Ref: any; //todo: strictly type this eventually...
-  video2Ref: any; //todo: strictly type this eventually...
-  video1RefIsLoaded: boolean;
-  video2RefIsLoaded: boolean;
-  setCurrentVideo1: (currentVideo: string) => void;
-  setCurrentVideo2: (currentVideo: string) => void;
-  setVideo1Ref: (videoRef: any) => void;
-  setVideo2Ref: (videoRef: any) => void;
-  playVideo1: () => void;
-  playVideo2: () => void;
-  pauseVideo1: () => void;
-  pauseVideo2: () => void;
-  goFullScreenOnElement: (elementId: string) => void;
+  switchVideo: () => void;
 }
 
 export const createVisualControlSlice: StateCreator<
@@ -29,67 +15,7 @@ export const createVisualControlSlice: StateCreator<
   [],
   VisualControlType
 > = (set, get) => ({
-  currentVideo1: coffeeVideo,
-  currentVideo2: skyScrapersVideo,
-  video1Hidden: false,
-  video2Hidden: false,
-  video1Ref: undefined,
-  video2Ref: undefined,
-  video1RefIsLoaded: false,
-  video2RefIsLoaded: false,
-
-  setCurrentVideo1: (currentVideo1) => {
-    set({ currentVideo1 });
-  },
-
-  setCurrentVideo2: (currentVideo2) => {
-    set({ currentVideo2 });
-  },
-
-  setVideo1Ref: (video1Ref) => {
-    set({ video1Ref, video1RefIsLoaded: true });
-    console.log("videoRef Loaded");
-  },
-
-  setVideo2Ref: (video2Ref) => {
-    set({ video2Ref, video2RefIsLoaded: true });
-    console.log("video2Ref Loaded");
-  },
-
-  playVideo1: () => {
-    const video1Ref = get().video1Ref;
-
-    console.log("playVideo from state");
-
-    video1Ref.play();
-  },
-
-  playVideo2: () => {
-    const video2Ref = get().video1Ref;
-
-    console.log("playVideo from state");
-
-    video2Ref.play();
-  },
-
-  pauseVideo1: () => {
-    const video1Ref = get().video1Ref;
-
-    console.log("pauseVideo from state");
-
-    video1Ref.pause();
-  },
-
-  pauseVideo2: () => {
-    const video2Ref = get().video2Ref;
-
-    console.log("pauseVideo2 from state");
-
-    video2Ref.pause();
-  },
-
-  goFullScreenOnElement: (elementId: string) => {
-    console.log("should be going fullscreen");
-    document.getElementById(elementId)?.requestFullscreen();
+  switchVideo: () => {
+    console.log("switchVideo called");
   },
 });
--- pi-client/src/store/useStore.ts
+++ pi-client/src/store/useStore.ts
@@ -1,6 +1,15 @@
-import create from 'zustand';
-import { createVisualControlSlice, VisualControlType } from "./createVisualControlSlice";
+import create from "zustand";
+import {
+  createVisualControlSlice,
+  VisualControlType,
+} from "./createVisualControlSlice";
+import { createVideo1Slice, Video1Type } from "./createVideo1Slice";
+import { createVideo2Slice, Video2Type } from "./createVideo2Slice";
 
-export const useStore = create<VisualControlType>()((...a) => ({
+export const useStore = create<VisualControlType & Video1Type & Video2Type>()(
+  (...a) => ({
     ...createVisualControlSlice(...a),
-  }));
+    ...createVideo1Slice(...a),
+    ...createVideo2Slice(...a),
+  })
+);
