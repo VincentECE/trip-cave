2ba4cffaf2cba013dd3ca4138a0dd01efcee8f86
Vince <vincent.tran.ece@gmail.com>
2022-12-25 19:44:04 -0600

    creates public folder to serve thumbnail images

--- pi-server/app-values.ts
+++ pi-server/app-values.ts
@@ -1,4 +1,5 @@
 export const PI_CLIENT_PORT = 3001;
 export const MOBILE_CLIENT_PORT = 3002;
+export const IMAGE_HOST_PORT = 3003;
 
 //todo: change this file to a .env if needed
--- pi-server/index.ts
+++ pi-server/index.ts
@@ -1,16 +1,22 @@
 import express from "express";
 require("dotenv").config();
-import { PI_CLIENT_PORT, MOBILE_CLIENT_PORT } from "./app-values";
+import {
+  PI_CLIENT_PORT,
+  MOBILE_CLIENT_PORT,
+  IMAGE_HOST_PORT,
+} from "./app-values";
 import { piClientHandler, mobileClientHandler } from "./src/socket-handlers";
 const app = express();
 import http from "http";
 const piClient_server = http.createServer(app);
 const mobileClient_server = http.createServer(app);
+const content_server = http.createServer(app);
 import { Server, Socket } from "socket.io";
 import { Console } from "console";
 // require("./db");
 // const PORT = process.env.PORT; //todo: add .env file
-app.use(express.static(__dirname + "../mobile-client/dist"));
+// app.use(express.static(__dirname + "../mobile-client/dist"));
+app.use(express.static("public/thumbnail-images"));
 
 const piClient_io: Server = new Server(piClient_server, {
   cors: {
@@ -54,6 +60,11 @@ piClient_server.listen(PI_CLIENT_PORT, () => {
 
 mobileClient_server.listen(MOBILE_CLIENT_PORT, () => {
   console.log(`mobileClient listening on ${MOBILE_CLIENT_PORT}`);
+});
+
+content_server.listen(IMAGE_HOST_PORT, () => {
+  // where thumbnail images are hosted from
+  console.log(`content host listening on :${IMAGE_HOST_PORT}`);
 });
 
 // app.listen(
