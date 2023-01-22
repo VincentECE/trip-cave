cf903d06163d2ff8b751c6c393569c0fc1d13b30
Vince <vincent.tran.ece@gmail.com>
2023-01-21 20:41:08 -0600

    adds comment

--- app-values.ts
+++ app-values.ts
@@ -5,7 +5,7 @@ export const MOBILE_CLIENT_PORT = 3002;
     Comment one of these out. It controls where all 3 repos are looking as host
 */
 
-export const CURRENT_HOST_IP = "http://192.168.86.34:";
+export const CURRENT_HOST_IP = "http://192.168.86.34:"; //todo: since we aren't serving the files through express, this needs to change to match whever vite is hosting the clients
 // export const CURRENT_HOST_IP = "http://localhost:";
 
 /************************/
