2855d26063fe3f8d9cadb02dd670250065450b18
Vince <vincent.tran.ece@gmail.com>
2023-02-19 18:33:55 -0600

    creates poopy routes

--- mobile-client/src/routes/RootRoute.tsx
+++ mobile-client/src/routes/RootRoute.tsx
@@ -1,5 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, RouteProps } from "react-router-dom";
+
+type CustomRouteProps = RouteProps & { exact?: boolean };
 
 export const RootRoute = (): JSX.Element => {
-  return <div>poop</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home/elsehere" element={<p>POOOOOP</p>} />
+        <Route path="/home/:something" element={<p>POOOOOP</p>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
