298f2c1c6df61ab3898007b52215f2cc9e37491b
Sandy Tran <just.sandy.tran@gmail.com>
2023-02-26 19:16:43 -0600

    feat: adding more colors to theme

--- mobile-client/src/style/colors.ts
+++ mobile-client/src/style/colors.ts
@@ -3,16 +3,22 @@ export type Theme = "dark" | "light"
 export type ColorTheme = "dark" | "light" | "appBg"
 
 export const lightModeColorTheme = {
-  black: 'black',
-  white: 'white',
-  appBg: 'white',
+  black: '#080D0E',
+  white: '#E2DED3',
+  appBg: '#0D1D26',
+  slateGray: "#2D353A",
+  mossGreen: "#7E837A",
+  taupeBrown: "#D4D1C0",
 };
 
 
 export const darkModeColorTheme = {
-  black: 'black',
-  white: 'white',
-  appBg: 'black',
+  black: '#080D0E',
+  white: '#E2DED3',
+  appBg: "#0D1D26",
+  slateGray: "#2D353A",
+  mossGreen: "#7E837A",
+  taupeBrown: "#D4D1C0",
 };
 
 
